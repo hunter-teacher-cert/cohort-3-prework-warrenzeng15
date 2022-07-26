cab2737efc9906515ee2faa681e5155bc4151c92
warrenzeng <96797919+warrenzeng15@users.noreply.github.com>
2022-07-26 20:45:11 +0000

    finished Celsius.java and Guess.java, created pre03 folder for the next part of prework

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,17 +1,17 @@
+import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 
 public class Celsius {
 
     public static void main(String[] args) {
-        String line;
+        double celsius;
+        double farenheit;
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Enter a temperature in: ");
-        line = in.nextLine();
-        System.out.println("You said: " + line);
-
-        System.out.print("Type something else: ");
-        line = in.nextLine();
-        System.out.println("You also said: " + line);
+        System.out.println("Enter a temperature in celsius: ");
+        celsius = in.nextDouble();
+        farenheit = celsius * 1.8 + 32;
+        System.out.printf("%.1f C = %.1f F\n", celsius,farenheit);
     }
 }
