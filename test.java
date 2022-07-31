f63d2801db13d3b7e01caf48670db09af1f2250a
warrenzeng <96797919+warrenzeng15@users.noreply.github.com>
2022-07-31 21:06:59 +0000

    messed around with test.java

--- test.java
+++ test.java
@@ -1,44 +1,17 @@
-import java.util.Scanner;
-
 public class test {
-  
-public static void nLines(int n) {
-    if (n > 0) {
-        System.out.println(1);
-        nLines(n - 1);
-    }
-}
-public static void zoop(String fred, int bob) {
-    System.out.println(fred);
-    if (bob == 5) {
-        ping("not ");
+
+  public static void count(int n) {
+    if (n == 0) {
+      System.out.println("Blastoff!");
     } else {
-        System.out.println("!");
+      System.out.println(n);
+      count(n - 1);
+      System.out.println(n);
+      
     }
-}
+  }
 
-public static void main(String[] args) {
-    int bizz = 5;
-    int buzz = 2;
-    nLines(4);
-    //zoop("just for", bizz);
-    //clink(2 * buzz);
+  public static void main(String[] args) {
+    count(5);
+  }
 }
-public static void clink(int fork) {
-    System.out.print("It's ");
-    zoop("breakfast ", fork);
-}
-
-public static void ping(String strangStrung) {
-    System.out.println("any " + strangStrung + "more ");
-}
-}
-
-    //countup(userguess);
-  
-    //public static void countup(int n) {
-    //if (n == 0) {
-    //    System.out.println("Blastoff!");
-    //} else {
-    //    countup(n - 1);
-    //    System.out.println(n);
